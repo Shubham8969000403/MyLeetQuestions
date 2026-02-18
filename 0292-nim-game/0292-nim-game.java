4619032283cae96bf5bc8420974103c20ed5bc25
Shubham <144883667+Shubham8969000403@users.noreply.github.com>
2026-02-18 23:45:43 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.54%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        boolean bool=true;
+        if(n%4==0){
+            bool=false;
+        }
+        return bool;
+            
+    }
+}
